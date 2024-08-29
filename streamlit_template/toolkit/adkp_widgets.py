34de2e828b199fd23efed1bc07a71c34bf9b0c6d
Abby Vander Linden <11965371+avanlinden@users.noreply.github.com>
2024-08-29 13:20:12 -0700

    update unique user plot

--- streamlit_template/toolkit/adkp_widgets.py
+++ streamlit_template/toolkit/adkp_widgets.py
@@ -1,9 +1,9 @@
 import pandas as pd
-import plotly.graph_objects as go # type: ignore
-from datetime import datetime
+import plotly.graph_objects as go
+from datetime import date
 
 
-def plot_unique_users(unique_users_df, width=2000, height=400):
+def plot_unique_users(unique_users_df, width=1800, height=600):
 
     # group by STUDY and MONTH, sum NUMBER OF UNIQUE USERS
     grouped_df = (
@@ -12,20 +12,22 @@ def plot_unique_users(unique_users_df, width=2000, height=400):
     .reset_index()
     )
 
+    grouped_df["MONTH"] = pd.to_datetime(grouped_df["MONTH"])
+
     # Sort studies by most unique users for current month
-    current_month = datetime.today().strftime(f"%Y-%m-01")
+    current_month = date.today().strftime(f"%Y-%m-01")
     recent_top_studies = (
-    grouped_df.loc[grouped_df["MONTH"] == current_month]
-    .sort_values(by="NUMBER_OF_UNIQUE_USERS", ascending=False)
-    .head(10)
-    )
+        grouped_df.loc[grouped_df["MONTH"] == current_month]
+        .sort_values(by="NUMBER_OF_UNIQUE_USERS", ascending=False)
+        .head(20)
+        )
 
     # Construct the plotly object
     fig = go.Figure()
-    for i, study in zip(recent_top_studies.index, recent_top_studies["STUDY"]):
+    for study in recent_top_studies["STUDY"]:
     
         # Extract data for current study
-        filtered_df = unique_users_df[unique_users_df["STUDY"].isin([study])]
+        filtered_df = unique_users_df[unique_users_df["STUDY"] == study]
         months = pd.to_datetime(filtered_df["MONTH"])
         counts = filtered_df["NUMBER_OF_UNIQUE_USERS"]
 
@@ -39,40 +41,42 @@ def plot_unique_users(unique_users_df, width=2000, height=400):
                 line=dict(width=2),
                 opacity=0.6,
                 hoverinfo="x+y+name",
-                hovertemplate="<b>Date</b>: %{x}<br><b>Users</b>: %{y}<extra></extra>",
+                hovertemplate= "<b>Date</b>: %{x}<br>" + "<b>Users</b>: %{y}<extra></extra>",
                 showlegend=True,
-                visible="legendonly",
+                visible="legendonly"
             )
         )
 
-        # Calculate monthly total for all studies
-        total_monthly_counts = (
-            unique_users_df.groupby("MONTH")["NUMBER_OF_UNIQUE_USERS"]
-            .sum()
-            .reset_index()
-            )
-
-        total_monthly_counts["MONTH"] = pd.to_datetime(total_monthly_counts["MONTH"])
-
-        # Add monthly totals to plot as default
-        fig.add_trace(
-            go.Scatter(
-                x=total_monthly_counts["MONTH"],
-                y=total_monthly_counts["NUMBER_OF_UNIQUE_USERS"],
-                mode="lines+markers",
-                name="All Studies",
-                line=dict(color="purple", width=4),
-                hoverinfo="x+y+name",
-                hovertemplate="<b>Date</b>: %{x}<br><b>Users</b>: %{y}<extra></extra>",
-                visible=True,
-            )
+    # Calculate monthly total for all studies
+    total_monthly_counts = (
+        unique_users_df.groupby("MONTH")["NUMBER_OF_UNIQUE_USERS"]
+        .sum()
+        .reset_index()
         )
 
-        fig.update_layout(
-            xaxis_title="Month",
-            yaxis_title="Unique Users",
-            title="Monthly Unique AD Portal Users (Click a Study to see details)",
-            width=width,
-            height=height,
+    total_monthly_counts["MONTH"] = pd.to_datetime(total_monthly_counts["MONTH"])
+
+    # Add monthly totals to plot as default
+    fig.add_trace(
+        go.Scatter(
+            x=total_monthly_counts["MONTH"],
+            y=total_monthly_counts["NUMBER_OF_UNIQUE_USERS"],
+            mode="lines+markers",
+            name="All Studies",
+            line=dict(color="purple", width=4),
+            hoverinfo="x+y+name",
+            hovertemplate="<b>Date</b>: %{x}<br><b>Users</b>: %{y}<extra></extra>",
+            visible=True,
+            showlegend=True
         )
-        return fig
+    )
+
+    fig.update_layout(
+        xaxis_title="Month",
+        yaxis_title="Unique Users",
+        title="Monthly Unique AD Portal Users (Click a Study to see details)",
+        legend=dict(title="Studies", orientation="v"),
+        width=width,
+        height=height
+    )
+    return fig
